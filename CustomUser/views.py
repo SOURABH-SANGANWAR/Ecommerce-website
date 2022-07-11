fa54103e56d3e198bed52ff43b23128180763027
SANGANWAR  SOURABH <79750636+SOURABH-SANGANWAR@users.noreply.github.com>
2022-07-11 22:31:23 +0530

    Update views.py

--- CustomUser/views.py
+++ CustomUser/views.py
@@ -2,9 +2,4 @@ from django.shortcuts import render
 from django.contrib.auth import login
 from CustomUser.models import CustomUser
 
-# Create your views here.
-# def Myfun(requests,*args,**kwargs):
-#     if(requests.POST):
-#         user = CustomUser.objects.get(id = 1)
-#         login(requests,user)
-#     return render(requests,'index.html',{'ranges' : range(5)})
+
