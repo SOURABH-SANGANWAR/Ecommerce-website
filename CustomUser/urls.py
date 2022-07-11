733d3b598511c1a2dafb68c661e2f6714ad1c461
SANGANWAR  SOURABH <79750636+SOURABH-SANGANWAR@users.noreply.github.com>
2022-07-11 22:31:47 +0530

    Update urls.py

--- CustomUser/urls.py
+++ CustomUser/urls.py
@@ -5,10 +5,5 @@ app_name = 'orders'
 
 
 urlpatterns = [
-    # path('<int:id>/', detail_view,name="detail_view"),
-    # path('update/<int:id>/', cview.as_view(),name = "update_view"),
-    # path('delete/<int:id>/', dview.as_view(),name = "delete_view"),
-    # path('create/', cview.as_view(), name = "create_view"),
-    # path('',lview.as_view(), name = "list_view"),
-    # path('/abcd',Myfun, name = "funt"),
+
 ]
